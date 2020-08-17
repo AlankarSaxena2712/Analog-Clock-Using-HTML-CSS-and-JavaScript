57f705e9875d9b6bc4f5e6409bbe2e6fa5579ec9
AlankarSaxena2712 <saxenaalankar42@gmail.com>
2020-08-17 08:29:12 +0530

    Smooth scroll of second hand

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ setInterval(() => {
     htime = date.getHours();
     mtime = date.getMinutes();
     stime = date.getSeconds();
+    mstime = date.getMilliseconds();
     hrotation = 30 * htime + mtime / 2;
     mrotation = 6 * mtime + 0.1 * stime;
-    srotation = 6 * stime;
-
+    srotation = 6 * stime + 6 * mstime / 1000;
     hour.style.transform = `rotate(${hrotation}deg)`;
     minute.style.transform = `rotate(${mrotation}deg)`;
     second.style.transform = `rotate(${srotation}deg)`;
-}, 1000)
+}, 1)
